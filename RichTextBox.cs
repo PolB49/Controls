316fa53c17cb8317cfadf4e35ce720956e2ceddf
PolB49 <158999860+PolB49@users.noreply.github.com>
2024-02-26 09:02:10 +0100

    RichTextBox Control

--- RichTextBox.cs
+++ RichTextBox.cs
@@ -1,6 +1,20 @@
- private void RichTextBox_TextChanged(object sender, EventArgs e)
- {
-     RichTextBox.AppendText("Text d'exemple de RichTextBox amb cursiva");
-     RichTextBox.SelectionFont = new System.Drawing.Font("Times New Roman", 10, System.Drawing.FontStyle.Italic);
-     System.Drawing.Image img = System.Drawing.Image.FromFile("richbox1.jpg");
- }
+using System.Windows.Forms;
+
+namespace Rich_Label
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ImageList();
+        }
+
+        private void RichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            RichTextBox.AppendText("Text d'exemple de RichTextBox amb cursiva");
+            RichTextBox.SelectionFont = new System.Drawing.Font("Times New Roman", 10, System.Drawing.FontStyle.Italic);
+            System.Drawing.Image img = System.Drawing.Image.FromFile("richbox1.jpg");
+        }
+    }
+}
